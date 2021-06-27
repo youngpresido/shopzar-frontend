3b37c74dc8f4e94fb3194b71bfbc56ea7e0bee33
chinwe.okonkwo <chinwe.okonkwo@gtbank.com>
2021-06-27 03:17:57 +0100

    add sidebar

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MaterialUiModule } from '../utilities/material-ui.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavigationComponent,
+    MainToolbarComponent,
+    UserDetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialUiModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [
+    NavigationComponent,
+    MainToolbarComponent,
+    UserDetailsComponent
   ]
 })
 export class ComponentModule { }
--- src/app/pages/market/market.module.ts
+++ src/app/pages/market/market.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MarketRoutingModule } from './market-routing.module';
 import { MarketComponent } from './market/market.component';
+import { MaterialUiModule } from 'src/app/utilities/material-ui.module';
+import { ComponentModule } from 'src/app/component/component.module';
 
 
 @NgModule({
   declarations: [MarketComponent],
   imports: [
     CommonModule,
-    MarketRoutingModule
+    MarketRoutingModule,
+    MaterialUiModule,
+    ComponentModule
   ]
 })
 export class MarketModule { }
