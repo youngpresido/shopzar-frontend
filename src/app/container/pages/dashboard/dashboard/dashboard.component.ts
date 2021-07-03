73c51c2c383717ef5d832f90c15dbaa1e605e8e2
chinwe.okonkwo <chinwe.okonkwo@gtbank.com>
2021-07-03 19:04:37 +0100

    commit

--- src/app/container/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/container/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-
 export interface PeriodicElement {
   name: string;
-  position: number;
+  icon: string;
   weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {icon: 'face', name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {icon: 'face', name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {icon: 'face', name: 'Lithium', weight: 6.941, symbol: 'Li'},
 ];
 
 
@@ -21,7 +20,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['icon', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
   clickedRows = new Set<PeriodicElement>();
   constructor() { }
