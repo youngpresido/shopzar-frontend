72d4e84d8aa8168a140e9e136ffac4d544f39df8
mac <mac@Teewaniis-MacBook-Pro.local>
2021-07-04 04:11:18 +0100

    a

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -30,24 +30,24 @@ export class DashComponent {
   // );
 
   cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return {
-          columns: 1,
-          miniCard: { cols: 1, rows: 1 },
-          chart: { cols: 1, rows: 2 },
-          table: { cols: 1, rows: 4 },
-        };
-      }
+   map(({ matches }) => {
+     if (matches) {
+       return {
+         columns: 1,
+         miniCard: { cols: 1, rows: 1 },
+         chart: { cols: 1, rows: 2 },
+         table: { cols: 1, rows: 4 },
+       };
+     }
 
-     return {
-        columns: 4,
-        miniCard: { cols: 1, rows: 1 },
-        chart: { cols: 2, rows: 2 },
-        table: { cols: 4, rows: 4 },
-      };
-    })
-  );
+    return {
+       columns: 4,
+       miniCard: { cols: 1, rows: 1 },
+       chart: { cols: 2, rows: 2 },
+       table: { cols: 4, rows: 4 },
+     };
+   })
+ );
 
 
   constructor(private breakpointObserver: BreakpointObserver) {}
